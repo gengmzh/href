4757e9c0aeec47713dfa1e3826c2e935dd0eabdc
gengmzh <gengmzh@gmail.com>
2013-01-28 15:24:38 +0800

    add logrotate

--- href-crawler/src/main/resources/bin/crawler.sh
+++ href-crawler/src/main/resources/bin/crawler.sh
@@ -5,7 +5,7 @@ JAVA='java'
 JOB_JAR=/root/href/href-crawler/href-crawler-0.1.0-jar-with-dependencies.jar
 MAIN_CLASS=cn.seddat.href.crawler.CrawlerScheduler
 LOG_CONFIG=/var/app/href/crawler/logging.properties
-LOG_FILE=/root/href/href-crawler/logs/crawler.`date +"%Y%m%d"`.log
+LOG_FILE=/root/href/href-crawler/logs/crawler.log
 echo 'jar '$JOB_JAR
 echo 'main class '$MAIN_CLASS
 echo 'log config '$LOG_CONFIG
