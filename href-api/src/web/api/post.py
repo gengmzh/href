d50f6b586a2a2896ef6ff1fc5dcdee8c76b39b58
gengmzh <gengmzh@gmail.com>
2013-02-22 10:06:23 +0800

    refine post api; using Asia/Shanghai time zone;

--- href-api/src/web/api/post.py
+++ href-api/src/web/api/post.py
@@ -10,21 +10,22 @@ log = logging.getLogger('href-api')
 
 def index(request):
     # args
-    st = request.REQUEST.get('st', None)
-    if st:
-        st = fromTimestamp(st)
-    et = request.REQUEST.get('et', None)
-    if et:
-        et = fromTimestamp(et)
+    st = request.REQUEST.get('time', None)
+    item = request.REQUEST.get('item', None)
+    order = request.REQUEST.get('order', 0)
+    limit = request.REQUEST.get('limit', 20)
     # query
     q = {}
     if st:
-        q['ct'] = {'$gt': st}
-    if et:
-        q['ct'] = {'$lt': et} 
-    coll = getCollection('post')
+        st = fromTimestamp(st)
+        order = '$gt' if order==0 else '$lt'
+        if item:
+            q['$or'] = [{'ct':{order: st}}, {'ct':st, '_id':{order: item}}]
+        else:
+            q['ct'] = {order: st}
     posts = []
-    cursor = coll.find(q, fields={'ctt':0}).sort([('ct', -1)]).limit(20)
+    coll = getCollection('post')
+    cursor = coll.find(q, fields={'ctt':0, 'sl':0, 'pt':0}).sort([('ct', -1), ('_id', -1)]).limit(limit)
     for post in cursor:
         posts.append(post)
     return HttpResponse(json.dumps(posts, ensure_ascii=False), content_type="application/json; charset=UTF-8")
