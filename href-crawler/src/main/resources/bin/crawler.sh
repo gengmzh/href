491559f3df99d80c4f00a9fb91ad529e6c655321
gengmzh <gengmzh@gmail.com>
2013-02-22 15:28:01 +0800

    refine start script

--- href-crawler/src/main/resources/bin/crawler.sh
+++ href-crawler/src/main/resources/bin/crawler.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
-echo 'starting crawler...'
 JAVA='java'
-JOB_JAR=/root/href/href-crawler/href-crawler-0.1.0-jar-with-dependencies.jar
+JOB_JAR=/var/app/href/crawler/href-crawler-0.1.0-jar-with-dependencies.jar
 MAIN_CLASS=cn.seddat.href.crawler.CrawlerScheduler
 LOG_CONFIG=/var/app/href/crawler/logging.properties
-LOG_FILE=/root/href/href-crawler/logs/crawler.log
+LOG_FILE=/var/log/href/crawler/crawler.log
 echo 'jar '$JOB_JAR
 echo 'main class '$MAIN_CLASS
 echo 'log config '$LOG_CONFIG
 echo 'log file '$LOG_FILE
+
+echo 'starting crawler...'
 
 nohup $JAVA -cp $JOB_JAR -Djava.util.logging.config.file=$LOG_CONFIG $MAIN_CLASS >>$LOG_FILE 2>&1 &
 
