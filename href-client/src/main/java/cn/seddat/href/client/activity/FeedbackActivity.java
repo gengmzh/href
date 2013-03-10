1a368ca139f546962fedea4cf2370cd17e466ad9
gengmzh <gengmzh@gmail.com>
2013-03-10 17:58:20 +0800

    add feedback

---
+++ href-client/src/main/java/cn/seddat/href/client/activity/FeedbackActivity.java
@@ -0,0 +1,46 @@
+package cn.seddat.href.client.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RatingBar;
+import android.widget.Toast;
+import cn.seddat.href.client.R;
+import cn.seddat.href.client.service.ToastService;
+
+public class FeedbackActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		this.setContentView(R.layout.feedback);
+	}
+
+	public void sendAdvice(View view) {
+		EditText advice = (EditText) findViewById(R.id.feedback_advice);
+		if (advice.getText().length() == 0) {
+			advice.setHint(R.string.feedback_advice_error_message);
+			return;
+		}
+		RatingBar rating = (RatingBar) findViewById(R.id.feedback_rating);
+		EditText email = (EditText) findViewById(R.id.feedback_email);
+		//TODO
+		ToastService.toast(this, rating.getRating() + "\n" + email.getText() + "\n" + advice.getText(),
+				Toast.LENGTH_SHORT);
+		// reset
+		advice.setText("");
+		advice.setHint(R.string.feedback_advice_hint);
+	}
+
+	@Override
+	public void onBackPressed() {
+		Activity parent = getParent();
+		if (parent != null) {
+			parent.onBackPressed();
+		} else {
+			super.onBackPressed();
+		}
+	}
+
+}
--- href-client/src/main/java/cn/seddat/href/client/activity/HrefActivity.java
+++ href-client/src/main/java/cn/seddat/href/client/activity/HrefActivity.java
@@ -1,10 +1,12 @@
 package cn.seddat.href.client.activity;
 
+import android.app.Activity;
 import android.app.ActivityGroup;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.widget.TableLayout;
 import android.widget.Toast;
 import cn.seddat.href.client.R;
@@ -27,6 +29,8 @@ public class HrefActivity extends ActivityGroup {
 		this.onMenuClick(findViewById(R.id.sideslipping_menu_default));
 	}
 
+	private boolean isMain;
+
 	public void onMenuClick(View view) {
 		TableLayout menus = (TableLayout) view.getParent();
 		for (int i = 0; i < menus.getChildCount(); i++) {
@@ -36,7 +40,7 @@ public class HrefActivity extends ActivityGroup {
 		view.setBackgroundColor(R.color.more_darker_gray);
 		switch (view.getId()) {
 		case R.id.sideslipping_menu_default:
-			this.showPostList(view);
+			this.startContent(PostActivity.class);
 			break;
 		case R.id.sideslipping_menu_recommend:
 			ToastService.toast(this, "敬请期待", Toast.LENGTH_SHORT);
@@ -45,7 +49,7 @@ public class HrefActivity extends ActivityGroup {
 			ToastService.toast(this, "敬请期待", Toast.LENGTH_SHORT);
 			break;
 		case R.id.sideslipping_menu_feedback:
-			ToastService.toast(this, "敬请期待", Toast.LENGTH_SHORT);
+			this.startContent(FeedbackActivity.class);
 			break;
 		case R.id.sideslipping_menu_about:
 			ToastService.toast(this, "敬请期待", Toast.LENGTH_SHORT);
@@ -53,27 +57,32 @@ public class HrefActivity extends ActivityGroup {
 		}
 	}
 
-	private void showPostList(View view) {
-		Intent i = new Intent(this, PostActivity.class);
-		View pl = getLocalActivityManager().startActivity(PostActivity.class.getName(), i).getDecorView();
-		slidingMenuView.showContent(pl);
+	private void startContent(Class<? extends Activity> activityClass) {
+		isMain = activityClass == PostActivity.class;
+		Intent i = new Intent(this, activityClass);
+		Window window = getLocalActivityManager().startActivity(activityClass.getName(), i);
+		slidingMenuView.showContent(window.getDecorView());
 	}
 
 	private long backTime = 0;
 
 	@Override
 	public void onBackPressed() {
-		if (slidingMenuView.isMenuShowing()) {
-			Log.i(tag, "hide left menu");
-			slidingMenuView.scrollRight();
-		} else {
-			long time = System.currentTimeMillis();
-			if (time - backTime > 2000) {
-				backTime = time;
-				ToastService.toast(this, "再按一次退出" + appName, Toast.LENGTH_SHORT);
+		if (isMain) {
+			if (slidingMenuView.isMenuShowing()) {
+				Log.i(tag, "hide left menu");
+				slidingMenuView.scrollRight();
 			} else {
-				super.onBackPressed();
+				long time = System.currentTimeMillis();
+				if (time - backTime > 2000) {
+					backTime = time;
+					ToastService.toast(this, "再按一次退出" + appName, Toast.LENGTH_SHORT);
+				} else {
+					super.onBackPressed();
+				}
 			}
+		} else {
+			this.onMenuClick(findViewById(R.id.sideslipping_menu_default));
 		}
 	}
 
