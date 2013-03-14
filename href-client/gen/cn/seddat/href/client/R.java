89482842f67af509dc12203b6c8748d7eb013918
gengmzh <gengmzh@gmail.com>
2013-03-14 18:29:53 +0800

    custom title

--- href-client/gen/cn/seddat/href/client/R.java
+++ href-client/gen/cn/seddat/href/client/R.java
@@ -25,37 +25,38 @@ public final class R {
         public static final int menu_mark=0x7f020005;
         public static final int menu_recommend=0x7f020006;
         public static final int refreshable_arrow=0x7f020007;
+        public static final int title_back=0x7f020008;
     }
     public static final class id {
-        public static final int author_icon=0x7f060014;
-        public static final int author_name=0x7f06000e;
-        public static final int feedback_advice=0x7f060002;
-        public static final int feedback_email=0x7f060001;
-        public static final int feedback_rating=0x7f060000;
-        public static final int post_company=0x7f06000f;
-        public static final int post_content=0x7f060010;
-        public static final int post_header=0x7f06000c;
-        public static final int post_list=0x7f060015;
-        public static final int post_mark=0x7f060012;
-        public static final int post_mark_desc=0x7f060011;
-        public static final int post_source=0x7f060013;
-        public static final int post_time=0x7f06000d;
-        public static final int post_title=0x7f06000b;
-        public static final int refreshable_arrow=0x7f06001a;
-        public static final int refreshable_footer=0x7f060016;
-        public static final int refreshable_header=0x7f060019;
-        public static final int refreshable_progress=0x7f060018;
-        public static final int refreshable_subtitle=0x7f06001c;
-        public static final int refreshable_text=0x7f06001b;
-        public static final int refreshable_title=0x7f060017;
-        public static final int sideslipping_content=0x7f06000a;
-        public static final int sideslipping_menu=0x7f060004;
-        public static final int sideslipping_menu_about=0x7f060009;
-        public static final int sideslipping_menu_default=0x7f060005;
-        public static final int sideslipping_menu_feedback=0x7f060008;
-        public static final int sideslipping_menu_mark=0x7f060007;
-        public static final int sideslipping_menu_recommend=0x7f060006;
-        public static final int sideslipping_view=0x7f060003;
+        public static final int author_icon=0x7f070014;
+        public static final int author_name=0x7f07000e;
+        public static final int feedback_advice=0x7f070002;
+        public static final int feedback_email=0x7f070001;
+        public static final int feedback_rating=0x7f070000;
+        public static final int post_company=0x7f07000f;
+        public static final int post_content=0x7f070010;
+        public static final int post_header=0x7f07000c;
+        public static final int post_list=0x7f070015;
+        public static final int post_mark=0x7f070012;
+        public static final int post_mark_desc=0x7f070011;
+        public static final int post_source=0x7f070013;
+        public static final int post_time=0x7f07000d;
+        public static final int post_title=0x7f07000b;
+        public static final int refreshable_arrow=0x7f07001a;
+        public static final int refreshable_footer=0x7f070016;
+        public static final int refreshable_header=0x7f070019;
+        public static final int refreshable_progress=0x7f070018;
+        public static final int refreshable_subtitle=0x7f07001c;
+        public static final int refreshable_text=0x7f07001b;
+        public static final int refreshable_title=0x7f070017;
+        public static final int sideslipping_content=0x7f07000a;
+        public static final int sideslipping_menu=0x7f070004;
+        public static final int sideslipping_menu_about=0x7f070009;
+        public static final int sideslipping_menu_default=0x7f070005;
+        public static final int sideslipping_menu_feedback=0x7f070008;
+        public static final int sideslipping_menu_mark=0x7f070007;
+        public static final int sideslipping_menu_recommend=0x7f070006;
+        public static final int sideslipping_view=0x7f070003;
     }
     public static final class layout {
         public static final int about=0x7f030000;
@@ -66,10 +67,17 @@ public final class R {
         public static final int post_list=0x7f030005;
         public static final int refreshable_footer=0x7f030006;
         public static final int refreshable_header=0x7f030007;
+        public static final int title_back=0x7f030008;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int feedback_advice_error_message=0x7f040002;
         public static final int feedback_advice_hint=0x7f040001;
+    }
+    public static final class style {
+        /**  深黑灰色 
+         */
+        public static final int href_tile=0x7f060000;
+        public static final int href_title_backgroud=0x7f060001;
     }
 }
--- href-client/src/main/java/cn/seddat/href/client/activity/AboutActivity.java
+++ href-client/src/main/java/cn/seddat/href/client/activity/AboutActivity.java
@@ -3,9 +3,11 @@
  */
 package cn.seddat.href.client.activity;
 
-import cn.seddat.href.client.R;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+import android.widget.TextView;
+import cn.seddat.href.client.R;
 
 /**
  * @author mzhgeng
@@ -16,7 +18,11 @@ public class AboutActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		this.requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
 		this.setContentView(R.layout.about);
+		this.getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.title_back);
+		TextView title = (TextView) this.findViewById(android.R.id.title);
+		title.setText("关于知遇");
 	}
 
 }
