477a62b3c1380fb394a9c0a5cba254e921e11401
gengmzh <gengmzh@gmail.com>
2013-08-04 01:07:15 +0800

    copy from href-crawler

---
+++ zhiyu-crawler/src/test/java/cn/seddat/zhiyu/crawler/spider/BaiduSpiderTest.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package cn.seddat.zhiyu.crawler.spider;
+
+import java.util.List;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+import cn.seddat.zhiyu.crawler.Post;
+
+/**
+ * @author mzhgeng
+ * 
+ */
+public class BaiduSpiderTest extends TestCase {
+
+	private BaiduSpider spider;
+
+	protected void setUp() throws Exception {
+		spider = new BaiduSpider();
+		spider.setCrawlAllPages(false);
+	}
+
+	public void testCrawl() throws Exception {
+		List<Post> pl = spider.crawl();
+		Assert.assertFalse(pl.isEmpty());
+		System.out.println("total: " + pl.size());
+		for (Post p : pl) {
+			System.out.println(p);
+		}
+	}
+
+}
